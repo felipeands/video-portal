35d052cadf03c1273276ebf8296a2b1654419870
Felipe Andrada <felipe.andrada@gmail.com>
2017-08-23 16:01:42 -0300

    update routes/config path

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+export class Config {
+
+  // API url
+  static get api(): string { return `http://localhost:3000/`; };
+
+  // user api
+  static get api_user(): string { return `${Config.api}user/auth`; };
+
+  // video api
+  static get api_videos(): string { return `${Config.api}videos` }
+
+
+}
