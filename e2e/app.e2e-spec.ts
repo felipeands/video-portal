ecdfbdc5bbcc532f1a45fa8f1771fa319555a9a7
Felipe Andrada <felipe.andrada@gmail.com>
2017-08-24 02:12:19 -0300

    update e2e routine

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('video-portal App', () => {
+describe('video-portal Login', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display form header', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Login');
   });
 });
